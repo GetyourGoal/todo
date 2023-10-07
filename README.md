03d961cfc5d68cc71dc63b72f8f222069d68a446
GetyourGoal <88654628+GetyourGoal@users.noreply.github.com>
2023-10-07 15:46:19 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# todo
+# todo-react
+Create a todo app using react and performed all CRUD operations.
---
+++ src/components/TodoForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const handleInput = (e) => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+  return (
+    <form className="TodoForm" onSubmit={handleInput}>
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="What is the task today?"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
+};
+
+export default TodoForm;
